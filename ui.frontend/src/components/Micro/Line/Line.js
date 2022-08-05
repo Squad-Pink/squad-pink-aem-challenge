af0c532795adcfcbd889f415a921eaea13afc80e
palomacha100 <palomacha100@gmail.com>
2022-08-04 22:29:19 -0300

    Feat: Line Micro Component created

---
+++ ui.frontend/src/components/Micro/Line/Line.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Line from "./Line.styled";
+
+const LineStyled = (lineColor) => {
+  return (
+    <div>
+      <Line />
+    </div>
+  );
+};
+
+export default LineStyled;
---
+++ ui.frontend/src/components/Micro/Line/Line.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Line = styled.hr`
+  border-top: solid 1px #aaaaaa;
+`;
+
+export default Line;
