03e36cfb3dc64ec657af44aca861f3a7deb09a55
palomacha100 <palomacha100@gmail.com>
2022-08-08 12:52:38 -0300

    Refactor: Changing Header test import

--- ui.frontend/src/components/import-components.js
+++ ui.frontend/src/components/import-components.js
@@ -20,7 +20,6 @@ import "./Container/Container";
 import "./ExperienceFragment/ExperienceFragment";
 import "./XxXxx";
 import MeuComponente from "./MeuComponente";
-import "./Header/Header";
 
 import { MapTo } from "@adobe/aem-react-editable-components";
 
