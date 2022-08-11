c88f410cdcf18319b13ca1abbe1d778f3667ff4e
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-08-11 15:28:30 -0300

    Refactor: changing the icons logic

--- ui.frontend/src/components/Micro/Button/Button.js
+++ ui.frontend/src/components/Micro/Button/Button.js
@@ -1,32 +1,13 @@
 import React from 'react'
 import { ButtonStyled } from './Button.styled';
 
-const Button = ({textButton, colorButton, type, firstIcon, secondIcon }) => {
-  switch (type) {
-    case "next":
-      firstIcon = "";
-      secondIcon = <i className="fa-solid fa-angle-right"></i>;
-    break;
-    case "more":
-      firstIcon = <i className="fa-solid fa-plus"></i>;
-      secondIcon = <i className="fa-solid fa-angle-right"></i>;
-    break;  
-    case "certificates":
-      firstIcon = "";
-      secondIcon = <i className="fa-solid fa-angle-down"></i>;
-    break;  
-    case "finish":
-      firstIcon = <i className="fa-solid fa-check"></i>;
-      secondIcon = "";
-    break;  
-    
-    default:
-      firstIcon="";
-      secondIcon = "";
-  };
-
+const Button = ({textButton, colorButton, id, firstIcon, secondIcon }) => { 
   return (
-    <ButtonStyled className={type} colorButton={colorButton}> {firstIcon} {textButton} {secondIcon} </ButtonStyled>
+    <ButtonStyled className={id} colorButton={colorButton}>
+      <img firstIcon={firstIcon} alt="icon" />
+       {textButton}  
+      <img secondIcon={secondIcon} alt="icon" />
+    </ButtonStyled>
   );
 }
 
