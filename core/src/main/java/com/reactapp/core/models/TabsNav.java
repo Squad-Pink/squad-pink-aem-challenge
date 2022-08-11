5cde25886db1e5786e0a662f273f92f18745a28a
Brunna Lage <brunnalage@gmail.com>
2022-08-11 17:08:49 -0300

    Feat: creation of tabs component in AEM and its dialog

---
+++ core/src/main/java/com/reactapp/core/models/TabsNav.java
@@ -0,0 +1,69 @@
+/*
+ * ***********************************************************************
+ * React App CONFIDENTIAL
+ * ___________________
+ *
+ * Copyright 2022 React App.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property
+ * of React App and its suppliers, if any. The intellectual and
+ * technical concepts contained herein are proprietary to React App
+ * and its suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from React App.
+ * ***********************************************************************
+ */
+
+package com.reactapp.core.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.osgi.annotation.versioning.ConsumerType;
+
+
+/**
+ * Defines the {@code TabsNav} Sling Model used for the {@code reactapp/components/tabs-nav} component.
+ * 
+ */
+@ConsumerType
+public interface TabsNav
+    extends ComponentExporter
+{
+
+
+    @JsonProperty("url1")
+    String getUrl1();
+
+    @JsonProperty("url2")
+    String getUrl2();
+
+    @JsonProperty("url3")
+    String getUrl3();
+
+    @JsonProperty("pageTitle")
+    String getPageTitle();
+
+    @JsonProperty("pageTitleColor")
+    String getPageTitleColor();
+
+    @JsonProperty("titleTab1")
+    String getTitleTab1();
+
+    @JsonProperty("titleTab2")
+    String getTitleTab2();
+
+    @JsonProperty("titleTab3")
+    String getTitleTab3();
+
+    @JsonProperty("tabTitleColor")
+    String getTabTitleColor();
+
+    @JsonProperty("titleBorderColor")
+    String getTitleBorderColor();
+
+    @JsonProperty("tabTitleActiveColor")
+    String getTabTitleActiveColorr();
+
+}
