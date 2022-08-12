749140a2e0267d96c75fb33c53e4f48a3bb677d2
Brunna Lage <brunnalage@gmail.com>
2022-08-12 11:48:42 -0300

    Refactor: changing button component styles

--- ui.frontend/src/components/Micro/Button/Button.styled.js
+++ ui.frontend/src/components/Micro/Button/Button.styled.js
@@ -1,25 +1,22 @@
 import styled from "styled-components";
-import {toRem} from '../../../utils/convertToRem';
+import { toRem } from "../../../utils/convertToRem";
 
 export const ButtonStyled = styled.button`
-    width: ${toRem(81)};
-    height: ${toRem(40)};
-    border: none;
-    border-radius: 4px;
-    background: ${(props) => props.colorButton};
-    font-family: 'Nunito';
-    font-style: normal;
-    font-weight: 400;
-    font-size: ${toRem(16)};
-    line-height: ${toRem(18)};
-    color: #FFFFFF;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    &.more {
-        width: ${toRem(109)};  
-    }
-    &.certificates {
-        width: ${toRem(129)};  
-    }
+  min-width: ${toRem(81)};
+  padding: ${toRem(11)};
+  height: ${toRem(40)};
+  gap: ${toRem(6)};
+  border: none;
+  border-radius: 4px;
+  background: ${(props) => props.colorButton};
+  font-family: "Nunito";
+  font-style: normal;
+  font-weight: 400;
+  font-size: ${toRem(16)};
+  line-height: ${toRem(18)};
+  color: #ffffff;
+  display: flex;
+  align-items: center; 
+  z-index: 50;
+  cursor: pointer;
 `;
