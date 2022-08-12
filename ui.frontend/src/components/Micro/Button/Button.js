b8ae674a6673252f704000e563105c866796d569
Brunna Lage <brunnalage@gmail.com>
2022-08-12 11:48:09 -0300

    Style: adding Button component props

--- ui.frontend/src/components/Micro/Button/Button.js
+++ ui.frontend/src/components/Micro/Button/Button.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import { ButtonStyled } from './Button.styled';
+import React from "react";
+import { ButtonStyled } from "./Button.styled";
 
-const Button = ({textButton, colorButton, id, firstIcon, secondIcon }) => { 
+const Button = ({
+  textButton,
+  colorButton,
+  id,
+  src,
+  src1,
+  onClick,
+  onBlur,
+  onMouseDown,
+}) => {
   return (
-    <ButtonStyled className={id} colorButton={colorButton}>
-      <img firstIcon={firstIcon} alt="icon" />
-       {textButton}  
-      <img secondIcon={secondIcon} alt="icon" />
+    <ButtonStyled
+      id={id}
+      colorButton={colorButton}
+      className={`${id}`}
+      onClick={onClick}
+      onBlur={onBlur}
+      onMouseDown={onMouseDown}
+    >
+      <img src={src} alt='icon' />
+      {textButton}
+      <img src={src1} alt='icon' />
     </ButtonStyled>
   );
-}
+};
 
 export default Button;
