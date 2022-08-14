a873e8f0201fef8319419767d8a42034d372ec15
Brunna Lage <brunnalage@gmail.com>
2022-08-13 21:41:14 -0300

    Feat: adding functional CertificatesInputBlock component

---
+++ ui.frontend/src/components/CertificatesInputBlock/CertificatesInputBlock.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import Label from '../Micro/Label/MicroLabel';
+import Input from '../Micro/Input/Input';
+import { InputDiv, LabelDiv } from './InputBlock.styled';
+import ErrorMessage from '../Micro/ErrorMessage/ErrorMessage';
+
+export const InputBlock = ({ 
+    register, 
+    label, 
+    labelColor, 
+    type, 
+    placeholderColor, 
+    id, 
+    borderColor, 
+    placeholder, 
+    value, 
+    text, 
+    colorButton, 
+    firstIcon, 
+    secondIcon, 
+    textButton, 
+    src, 
+    src1, 
+    errorText, 
+    colorError, 
+    errors, 
+    limitErrorText 
+    }) => {
+
+    const [btnActive, setBtnActive] = useState(false);
+    const activatorBtn = () => setBtnActive(true);
+
+    const [addedItem, setAddedItem] = useState([]);
+    const [itemName, setItemName] = useState("");
+
+    const [isActive, setIsActive] = useState(false);
+    const isError = () => setIsActive(!isActive);
+
+    const removeItem = (itemOption) => () => {
+        if (addedItem.length == 5) {
+            setAddedItem((addedItem) =>
+                addedItem.filter((i) => i !== itemOption)
+            );
+            isError();
+        } else {
+            setAddedItem((addedItem) =>
+                addedItem.filter((i) => i !== itemOption)
+            );
+        }
+    };
+
+    function handleChange(e) {
+        setItemName(e.target.value);
+    }
+
+    function handleClick() {
+        if (addedItem.length <= 4) {
+            setAddedItem([...addedItem, itemName]);
+            activatorBtn();
+        } else {
+            isError();
+        }
+    };
+
+    switch (type) {
+        case "text":
+            type = "text";
+            break;
+        case "url":
+            type = "url";
+            break;
+        case "tel":
+            type = "tel";
+            break;
+        case "email":
+            type = "email";
+            break;
+        default:
+            type = "aaa"
+    }
+
+    return (
+        <>
+            <InputDiv className={id}>
+                <LabelDiv>
+                    <Label label={label} labelColor={labelColor} id={id} />
+                </LabelDiv>
+                <Input placeholderColor={placeholderColor}
+                    borderColor={borderColor}
+                    id={id}
+                    placeholder={placeholder}
+                    type={type}
+                    value={value}
+                    onChange={handleChange}
+                    register={register}
+                />
+            </InputDiv>
+            <SplitButtonContainer className={`btnActive ${btnActive ? "active" : "inactive"}`}>
+                <SplitButton
+                    text={text}
+                    colorButton={colorButton}
+                    firstIcon={firstIcon}
+                    secondIcon={secondIcon}
+                    id="list"
+                    list={addedItem.map((item) => (
+                        <>
+                            <LiStyled key={item + 1}>{item}</LiStyled>
+                            <RemoveBtn
+                                type="button"
+                                id="Remove"
+                                title="...&emsp;X"
+                                onMouseDown={removeItem(item)}
+                            />
+                        </>
+                    ))}
+                />
+            </SplitButtonContainer>
+            <MoreBtnContainer>
+                <Button
+                    textButton={textButton}
+                    colorButton={colorButton}
+                    src={src}
+                    src1={src1}
+                    type="button"
+                    id="More"
+                    onClick={handleClick} />
+                <ErrorMessage type={type} errorText={errorText} colorError={colorError} errors={errors}  />
+                <CertificatesErrorMessage className={`limit ${isActive ? "active" : "inactive"}`}>
+                    {limitErrorText}</CertificatesErrorMessage>
+            </MoreBtnContainer>
+        </>
+    )
+}
