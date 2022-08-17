eea62eec076f83348ea1949668ae709c09ee45c5
Brunna Lage <brunnalage@gmail.com>
2022-08-16 22:20:47 -0300

    Refactor: change in age calculation logic and validation logic

--- ui.frontend/src/components/Birthday/Birthday.js
+++ ui.frontend/src/components/Birthday/Birthday.js
@@ -2,56 +2,62 @@ import React, { useState, useEffect } from "react";
 import Select from '../Micro/Select/Select';
 import { Result, BirthdayContainer, LabelContainer, DateContainer } from './Birthday.styled';
 import Label from '../Micro/Label/MicroLabel';
+import ErrorMessage from "../Micro/ErrorMessage/ErrorMessage";
 
-const Birthday = ({labelColor, labelTitle, labelMonth, labelDay, borderColor, labelYear}) => {
-  const [day, setDay] = useState();
-  const [month, setMonth] = useState();
-  const [year, setYear] = useState(0);
-  const [age, setAge] = useState(0);
+const Birthday = ({ labelColor, labelTitle, labelMonth, labelDay, borderColor, labelYear, labelAge, register, errors }) => {
+  const [dayOption, setDayOption] = useState(0);
+  const [monthOption, setMonthOption] = useState(0);
+  const [yearOption, setYearOption] = useState(0);
+  const [age, setAge] = useState();
+  
+  function changeDay(e) {
+    setDayOption(e.target.value);
+  }
 
-  useEffect(() => {
-    if (year != "") {
-      changeAge(year);
+  function changeMonth(e) {
+    setMonthOption(e.target.value);
+  }
+
+  function changeYear(e) {
+    setYearOption(e.target.value)    
+  }
+
+  function calculaAge() {
+    getAge()
+  }
+  function getAge() {
+    var today = new Date();    
+    var age = today.getFullYear() - yearOption;
+    var m = today.getMonth() - monthOption;
+    if (m < 0 || (m === 0 && today.getDate() < dayOption)) {
+      age--;     
     }
-  }, [year]);
-  useEffect(() => {
-    if (year != "") {
-      changeAge(year);
-    }
-  }, [day]);
-  useEffect(() => {
-    if (year != "") {
-      changeAge(year);
-    }
-  }, [month]);
-  const changeAge = (currentYear) => {
-    setAge(2022 - currentYear);
-    console.log(age);
-    ;
-  };
+    setAge(age);
+  } 
 
   return (
     <BirthdayContainer>
       <LabelContainer>
-        <Label labelColor={labelColor} id="Birthday">{labelTitle}</Label>
+        <Label labelColor={labelColor} id="Birthday" label={labelTitle}/>
+        <ErrorMessage errorText="TA ERRADO FIDAMAE" colorError={"red"} errors={errors} id="Age"/>
       </LabelContainer>
       <DateContainer>
         <div>
-          <Label labelColor={labelColor} id="inputs">{labelDay}</Label>
-          <Select id="Day" borderColor={borderColor} setDay={setDay} />
+          <Label labelColor={labelColor} id="inputs"label={labelDay}/>
+          <Select id="Day" borderColor={borderColor} onInput={changeDay} />
         </div>
         <div>
-          <Label labelColor={labelColor} id="inputs">{labelMonth}</Label>
-          <Select id="Month" borderColor={borderColor} setMonth={setMonth} />
+          <Label labelColor={labelColor} id="inputs" label={labelMonth}/>
+          <Select id="Month" borderColor={borderColor} onInput={changeMonth} />
         </div>
         <div>
-          <Label labelColor={labelColor} id="inputs">{labelYear}</Label>
-          <Select id="Year" borderColor={borderColor} setYear={setYear} />
+          <Label labelColor={labelColor} id="inputs" label={labelYear}/>
+          <Select id="Year" borderColor={borderColor} onInput={changeYear} onBlur={calculaAge} />
         </div>
         <div>
-          <Label labelColor={labelColor} id="inputs" label="Age" />
-          <Result borderColor={borderColor} id="Age" placeholder="Age" type="text" value={age} disabled />
-        </div>
+          <Label labelColor={labelColor} id="inputs" label={labelAge} />
+          <Result borderColor={borderColor} id="Age" placeholder="Age" type="text" value={age} {...register("Age")} disabled/>
+        </div>        
       </DateContainer>
     </BirthdayContainer>
   )
--- ui.frontend/src/components/XxXxx/index.js
+++ ui.frontend/src/components/XxXxx/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { MapTo } from "@adobe/aem-react-editable-components";
+import Birthday from "../Birthday/Birthday";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 const XxXxxEditConfig = {
   emptyLabel: "Digite aqui o texto por favor",
@@ -9,12 +13,31 @@ const XxXxxEditConfig = {
   },
 };
 
+const schema = yup.object({
+  Age: yup.number().required(),
+}).required();
+
 const XxXxx = (props) => {
+  const { register, handleSubmit, formState:{ errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = data => console.log(data);
+
   return (
-    <div>
-      <p>My Text Component: {props.text}</p>
-      <p>My Checkbox is: {props.checkbox ? "checked" : "unchecked"}</p>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>     
+      <Birthday 
+      register={register}
+      errors={errors}
+      labelColor={"black"}
+      labelTitle="Birthday"
+      labelMonth="Month"
+      labelDay="Day"
+      borderColor={"Black"}
+      labelYear="Year"
+      labelAge="Age"/>
+      <button type="submit">ENVIA DIABO</button>
+    </form>
   );
 };
 
